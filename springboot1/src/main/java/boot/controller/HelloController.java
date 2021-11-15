97e6b81298148885def8a004add6019ca965a0cd
root <root@localhost.localdomain>
2021-11-15 14:10:10 +0800

    .

--- springboot1/src/main/java/boot/controller/HelloController.java
+++ springboot1/src/main/java/boot/controller/HelloController.java
@@ -1,5 +1,6 @@
 package boot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
  * User: zookao
  * Date: 2021-11-12
  */
+@Slf4j
 @RestController
 public class HelloController {
     @RequestMapping(value = "/index")
     public String index(){
-        return "Hello world";
+        log.info("lombok的slf4j日志记录功能");
+        return "Hello world333";
     }
 }
